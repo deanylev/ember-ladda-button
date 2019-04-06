9fd21fa4ae618abed3df64cda7e23129afca49a4
Dean Levinson <dean.e.levinson@gmail.com>
2019-04-06 18:53:38 +1100

    Fix spinner not animating

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -7,6 +7,9 @@ module.exports = function(defaults) {
     // Add options here
   });
 
+  app.import('vendor/spin.min.js');
+  app.import('vendor/ladda.min.js');
+
   /*
     This build file specifies the options for the dummy test app of this
     addon, located in `/tests/dummy`
