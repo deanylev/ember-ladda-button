4e5bd36c05083932cb7afac1f82c88eea198f39f
sly7-7 <sylvain.mina@gmail.com>
2021-02-25 14:44:25 +0100

    don't execute the action on click if the button is disabled

--- tests/integration/components/ladda-button-test.js
+++ tests/integration/components/ladda-button-test.js
@@ -1,12 +1,13 @@
 import { module, test } from 'qunit';
 import { setupRenderingTest } from 'ember-qunit';
-import { render } from '@ember/test-helpers';
+import { click, render } from '@ember/test-helpers';
+
 import hbs from 'htmlbars-inline-precompile';
 
-module('Integration | Component | ladda-button', function(hooks) {
+module('Integration | Component | ladda-button', function (hooks) {
   setupRenderingTest(hooks);
 
-  test('it renders', async function(assert) {
+  test('it renders', async function (assert) {
     // Set any properties with this.set('myProperty', 'value');
     // Handle any actions with this.set('myAction', function(val) { ... });
 
@@ -22,5 +23,21 @@ module('Integration | Component | ladda-button', function(hooks) {
     `);
 
     assert.equal(this.element.textContent.trim(), 'template block text');
+  });
+
+  test("it doesn't send the action if already running", async function (assert) {
+    assert.expect(1);
+    this.set('execute', async () => {
+      assert.ok(true, 'action is called');
+    });
+
+    await render(hbs`
+      {{#ladda-button class="test-button" action=this.execute}}
+        template block text
+      {{/ladda-button}}
+    `);
+
+    click('.test-button');
+    await click('.test-button');
   });
 });
